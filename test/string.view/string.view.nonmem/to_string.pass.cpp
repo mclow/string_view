9d2803c7505779e6a323f04674bc7bc694f564bb
Marshall Clow <marshall@idio.com>
2014-06-01 20:43:57 -0700

    Update interface to match N4023

--- test/string.view/string.view.nonmem/to_string.pass.cpp
+++ test/string.view/string.view.nonmem/to_string.pass.cpp
@@ -10,31 +10,41 @@
 
 // <string_view>
 
+//  template<class Allocator>
+//  explicit operator basic_string<_CharT, _Traits, Allocator> () const;
 //  template<class _CharT, class _Traits = char_traits<_CharT>, class Allocator = allocator<_CharT> >
 //  basic_string<_CharT, _Traits, Allocator> to_string (
-//          basic_string_view<_CharT, _Traits> _sv, const Allocator& _a = Allocator())
+//          basic_string_view<_CharT, _Traits> _sv, const Allocator& _a = Allocator()) const;
 
 #include <experimental/string_view>
 #include <cassert>
+#include "min_allocator.h"
 
 #if _LIBCPP_STD_VER > 11
 
 template<typename CharT>
 void test ( const CharT *s ) {
+    typedef std::basic_string<CharT> String ;
     {
     std::experimental::basic_string_view<CharT> sv1 { s };
-    std::basic_string<CharT>                    str { to_string(sv1) };
-
-    assert ( sv1.size() == str.size ());
-    assert ( std::char_traits<CharT>::compare ( sv1.data(), str.data(), sv1.size()) == 0 );
+    String                                      str1 = (String) sv1;
+    auto                                        str2 = sv1.to_string(min_allocator<CharT>());
+    
+    assert ( sv1.size() == str1.size ());
+    assert ( sv1.size() == str2.size ());
+    assert ( std::char_traits<CharT>::compare ( sv1.data(), str1.data(),  sv1.size()) == 0 );
+    assert ( std::char_traits<CharT>::compare ( sv1.data(), str2.data(), sv1.size()) == 0 );
     }
 
     {
     std::experimental::basic_string_view<CharT> sv1 {};
-    std::basic_string<CharT>                    str { to_string(sv1) };
+    String                                      str1 = (String) sv1;
+    auto                                        str2 = sv1.to_string(min_allocator<CharT>());
 
-    assert ( sv1.size() == str.size ());
-    assert ( std::char_traits<CharT>::compare ( sv1.data(), str.data(), sv1.size()) == 0 );
+    assert ( sv1.size() == str1.size ());
+    assert ( sv1.size() == str2.size ());
+    assert ( std::char_traits<CharT>::compare ( sv1.data(), str1.data(), sv1.size()) == 0 );
+    assert ( std::char_traits<CharT>::compare ( sv1.data(), str2.data(), sv1.size()) == 0 );
     }
 }
 
